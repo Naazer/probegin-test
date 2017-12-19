502f89cd84c4db49e63de82d8f75625c448ccd08
Nazar Salo <salo.nazar@gmail.com>
2017-12-19 17:13:45 +0200

    realize sort

--- php/Utils/Arrays.php
+++ php/Utils/Arrays.php
@@ -10,6 +10,14 @@ class Arrays
     protected $array;
 
     /**
+     * @param array $array
+     */
+    public function setArray(array $array): void
+    {
+        $this->array = $array;
+    }
+
+    /**
      * Given an array of integers and a desired sum,
      * return the first two occuring elements which could be added together to form the desired result.
      * @example Utils\Arrays::findPair(7, [1, 4, 5, 3, 6]) would return [4, 3] or [1, 6]
@@ -32,7 +40,66 @@ class Arrays
      */
     public function sortBy(callable $sorting): array
     {
-        // @TODO: create body of the function and cover with tests
+        if (!is_callable($sorting)) {
+            throw new \DomainException(sprintf(
+                'Sorting method <%s> that you provided are not realized or unknown', $sorting
+            ));
+        }
+
+        call_user_func($sorting);
+
+        return $this->array;
+    }
+
+    /**
+     * Sort by quickSort algorithm
+     * I use one function for start sorting and for recursion
+     * @param int $left
+     * @param int $right
+     */
+    private function quickSort(int $left = 0, int $right = 0): void
+    {
+        // Start sorting and prepare copy of variables
+        $right == 0 && $right = count($this->array) - 1;
+        $l = $left;
+        $r = $right;
+        // Calculate the 'center' (take the value of the central cell of the array)
+        $center = $this->array[(int)($left + $right) / 2];
+        do {
+            while ($this->array[$l] < $center) $l++; // looking for values less than the 'center'
+            while ($this->array[$r] > $center) $r--; // looking for values greater than the 'center'
+            // after passing the cycles, check the cycle counters
+            if ($l <= $r) {
+                // and if the condition is true, then change the cells with each other
+                $this->array[$l] > $this->array[$r] &&
+                    list($this->array[$l], $this->array[$r]) = [$this->array[$r], $this->array[$l]];
+                $l++;
+                $r--;
+            }
+        } while ($l <= $r); // repeat the loop if true
+        // if the condition is true than recursion; current start and end
+        $l < $right && $this->quickSort($l, $right);
+        // if the condition is true than recursion; initial start and current end
+        $r > $left && $this->quickSort($left, $r);
+    }
+
+    /**
+     * Sort by bubble sort algorithm
+     */
+    private function bubbleSort() {
+        // array size minus 1
+        $size = count($this->array) - 1;
+        // bubble sorting
+        for ($j = 0; $j < $size; $j++) {
+            // iteration
+            for ($i = 0; $i < ($size - $j); $i++) {
+                // comparison
+                if ($this->array[$i] > $this->array[$i + 1]) {
+                    // swap
+                    list($this->array[$i], $this->array[$i + 1]) = [$this->array[$i + 1], $this->array[$i]];
+                }
+            }
+        }
     }
 
     /**
